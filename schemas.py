9aa15ebd9b43ed470f5a3f2948fd27a357ae5ed8
OmShukla <om0807@gmail.com>
2026-01-16 20:05:33 +0530

    created the schemas for user related operations

---
+++ schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel,EmailStr
+from models import SystemRole
+
+#defining the schema for user related operations(like signup,login etc)
+#schema for signiup the new user:
+class UserBase(BaseModel):
+    email:EmailStr
+    full_name:str
+#schema for creating the user
+class UserCreate(UserBase):
+    password: str
+
+#schma for storing the user details once he is signed in
+class UserOut(UserBase):
+    id:int
+    system_role:SystemRole
+    class Config:
+        from_attributes = True # to tell pydantic that the data will come from an ORM model
+        #alternatively we can use orm_mode = True
+
+#schema for login
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
